bf3bd84cdb2da2d9586f409e1418dee05d070123
Ryan Huard <ryan.huard@live.ca>
2015-02-01 00:06:52 -0700

    Jump around, jump up and get down Jump around, jump around!

--- WiiLeviathan/WiiLeviathan.cs
+++ WiiLeviathan/WiiLeviathan.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Web.Helpers;
+using Newtonsoft.Json;
 using WiimoteLib;
 
 namespace WiiLeviathanConsole
 {
 	class WiiLeviathan
 	{
+
 		public static float getWeight(Wiimote wiimote)
 		{
 			return wiimote.WiimoteState.BalanceBoardState.WeightLb;
@@ -40,41 +45,66 @@ namespace WiiLeviathanConsole
 			weightCal = weightCal / 100;
 			xCal = xCal / 100;
 			yCal = yCal / 100;
-			int xSpeed=0;
-			int ySpeed=0;
+			int turn=0;
+			int speed=0;
             Console.Write("Ycal: ");
             Console.WriteLine(yCal.ToString());
+
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //This works allegedly.... 
+            byte[] byteaddress = new byte[] { 192, 168, 1, 100 };
+            IPAddress address = new IPAddress(byteaddress);
+
+            IPEndPoint endpoint = new IPEndPoint(address, 50007);
+            //....ignorant
+            socket.Connect(endpoint);
+            int jump = 0;
+
+
 			while (true) 
 			{
+                jump = 0;
+                //Get and print the Jump event and constantly updating x,y speeds with deadzones
 			    if(Math.Abs(getWeight(wiimote) - weightCal) >= 50)
 			    {
-				    Console.WriteLine("J");
+                    jump = 1;
 			    }
 			    if (getY(wiimote) < (yCal- 3))
 			    {
                    //This should be forward
-	 			   ySpeed = Math.Min(Convert.ToInt32(Math.Ceiling(getY(wiimote)+.5) * 255 / -12),255);
+	 			   speed = Math.Min(Convert.ToInt32(Math.Ceiling(getY(wiimote)+.5) * 255 / -12),255);
 			    }
 			    if (getY(wiimote) > (yCal + 3))
 			    {
                     //This should be Backwards
-				    ySpeed = Math.Max(Convert.ToInt32(Math.Floor(getY(wiimote) - .5) * 255 / -12), -255);
+				    speed = Math.Max(Convert.ToInt32(Math.Floor(getY(wiimote) - .5) * 255 / -12), -255);
 			    }
 			    if (getX(wiimote) > (xCal + 1))
 			    {
-				    xSpeed = Math.Max(Convert.ToInt32(Math.Floor((getX(wiimote) - .5) * 30 / 12)), -30);
+				    turn = Math.Max(Convert.ToInt32(Math.Floor((getX(wiimote) - .5) * 30 / 12)), -30);
 			    }
 			    if (getX(wiimote) < (xCal - 1))
 			    {
-				    xSpeed = Math.Min(Convert.ToInt32(Math.Ceiling((getX(wiimote) + .5 )* 30 /12)), 30);
+				    turn = Math.Min(Convert.ToInt32(Math.Ceiling((getX(wiimote) + .5 )* 30 /12)), 30);
 			    }
 			    Console.Write("X: ");
-			    Console.WriteLine(xSpeed.ToString());
+			    Console.WriteLine(turn.ToString());
 			    Console.Write("Y: ");
-			    Console.WriteLine(ySpeed.ToString());
-                System.Threading.Thread.Sleep(500);
+			    Console.WriteLine(speed.ToString());
+                Dictionary<string, int> dick = new Dictionary<string,int>();
+                dick.Add("speed", speed);
+                dick.Add("steer", turn);
+                dick.Add("aux", jump);
+                string JsonString = JsonConvert.SerializeObject(dick);
+                Console.WriteLine(JsonString);
                 
+
+                byte[] data = Encoding.ASCII.GetBytes(JsonString);
+                socket.Send(data);
+                System.Threading.Thread.Sleep(250);
 			}
+            socket.Close();
         }
     }
 }
