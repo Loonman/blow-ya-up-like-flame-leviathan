135b79ca9ce21f6f016e4be717c165a5ed24f514
Ryan Huard <ryan.huard@live.ca>
2015-01-31 21:22:08 -0700

    Wii Leviathan fully calibrated for computer engineer despite reluctance of subject. Deadly neurotoxin was helpful in experimentation.

--- WiiLeviathan/WiiLeviathan.cs
+++ WiiLeviathan/WiiLeviathan.cs
@@ -31,44 +31,49 @@ namespace WiiLeviathanConsole
 			float weightCal = 0;
 			float xCal = 0;
 			float yCal = 0;
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 100; i++)
 			{
 				weightCal += getWeight(wiimote);
 				xCal += getX(wiimote);
 				yCal += getY(wiimote);
-				System.Threading.Thread.Sleep(250);
 			}
-			weightCal = weightCal / 10;
-			xCal = xCal / 10;
-			yCal = yCal / 10;
-			Int16 xSpeed=0;
-			Int16 ySpeed=0;
+			weightCal = weightCal / 100;
+			xCal = xCal / 100;
+			yCal = yCal / 100;
+			int xSpeed=0;
+			int ySpeed=0;
+            Console.Write("Ycal: ");
+            Console.WriteLine(yCal.ToString());
 			while (true) 
 			{
 			    if(Math.Abs(getWeight(wiimote) - weightCal) >= 50)
 			    {
 				    Console.WriteLine("J");
 			    }
-			    if (getY(wiimote) < (yCal-.5))
+			    if (getY(wiimote) < (yCal- 3))
 			    {
-	 			   ySpeed = Convert.ToInt16(Math.Ceiling(getY(wiimote)+.5) * 255 / -12);
+                   //This should be forward
+	 			   ySpeed = Math.Min(Convert.ToInt32(Math.Ceiling(getY(wiimote)+.5) * 255 / -12),255);
 			    }
-			    if (getY(wiimote) > (yCal + .5))
+			    if (getY(wiimote) > (yCal + 3))
 			    {
-				    ySpeed = Convert.ToInt16(Math.Floor(getY(wiimote) - .5) * 255 / 12);
+                    //This should be Backwards
+				    ySpeed = Math.Max(Convert.ToInt32(Math.Floor(getY(wiimote) - .5) * 255 / -12), -255);
 			    }
-			    if (getX(wiimote) > (xCal + .5))
+			    if (getX(wiimote) > (xCal + 1))
 			    {
-				    xSpeed = Convert.ToInt16(Math.Floor((getX(wiimote) - .5) * 255 / 12));
+				    xSpeed = Math.Max(Convert.ToInt32(Math.Floor((getX(wiimote) - .5) * 30 / 12)), -30);
 			    }
-			    if (getX(wiimote) < (xCal - .5))
+			    if (getX(wiimote) < (xCal - 1))
 			    {
-				    xSpeed = Convert.ToInt16(Math.Ceiling((getX(wiimote) + .5 )*255/12));
+				    xSpeed = Math.Min(Convert.ToInt32(Math.Ceiling((getX(wiimote) + .5 )* 30 /12)), 30);
 			    }
 			    Console.Write("X: ");
 			    Console.WriteLine(xSpeed.ToString());
 			    Console.Write("Y: ");
 			    Console.WriteLine(ySpeed.ToString());
+                System.Threading.Thread.Sleep(500);
+                
 			}
         }
     }
