3ff4c4d1083dde9decc0164bb71ca546f77f10d4
Ryan Huard <ryan.huard@live.ca>
2015-02-01 03:25:15 -0700

    Now "multithreaded"

---
+++ WiiLeviathan/Flame Leviathan.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WiiLeviathanNowWithFlameLeviathan
+{
+    partial class FlameLeviathan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FlameLeviathan));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(118, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 290);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start Leviathan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FlameLeviathan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(564, 413);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FlameLeviathan";
+            this.Text = "Flame Leviathan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WiiLeviathan/Flame Leviathan.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiimoteLib;
+using System.Web.Helpers;
+using Newtonsoft.Json;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace WiiLeviathanNowWithFlameLeviathan
+{
+    public partial class FlameLeviathan : Form
+    {
+        public FlameLeviathan()
+        {
+            InitializeComponent();
+        }
+        
+		public static float getWeight(Wiimote wiimote)
+		{
+			return wiimote.WiimoteState.BalanceBoardState.WeightLb;
+		}
+		public static float getX(Wiimote wiimote)
+		{
+			return wiimote.WiimoteState.BalanceBoardState.CenterOfGravity.X;
+		}
+		public static float getY(Wiimote wiimote)
+		{
+			return wiimote.WiimoteState.BalanceBoardState.CenterOfGravity.Y;
+		}
+        public class leviathanserver
+        {
+            public void WiiLeviathanServer()
+            {
+                Wiimote wiimote = new Wiimote();
+                wiimote.Connect();
+                wiimote.SetReportType(InputReport.IRAccel, true);
+                wiimote.SetLEDs(true, true, true, true);
+                //Calibration loop
+                float weightCal = 0;
+                float xCal = 0;
+                float yCal = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    weightCal += getWeight(wiimote);
+                    xCal += getX(wiimote);
+                    yCal += getY(wiimote);
+                }
+                weightCal = weightCal / 100;
+                xCal = xCal / 100;
+                yCal = yCal / 100;
+                int turn = 0;
+                int speed = 0;
+                Console.Write("Ycal: ");
+                Console.WriteLine(yCal.ToString());
+
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                //This works allegedly.... 
+                byte[] byteaddress = new byte[] { 192, 168, 1, 100 };
+                IPAddress address = new IPAddress(byteaddress);
+
+                IPEndPoint endpoint = new IPEndPoint(address, 50007);
+                //....ignorant
+
+                Connect:
+                try
+                {
+                    socket.Connect(endpoint);
+                }
+                catch
+                {
+                    var result = MessageBox.Show("Failed to Connect, Trying again");
+                    goto Connect;      
+                }
+
+                bool jump = false;
+                while (true)
+                {
+                    jump = false;
+                    //Get and print the Jump event and constantly updating x,y speeds with deadzones
+                    if (Math.Abs(getWeight(wiimote) - weightCal) >= 50)
+                    {
+                        jump = true;
+                    }
+                    if (getY(wiimote) < (yCal - 1))
+                    {
+                        //This should be forward
+                        speed = Math.Min(Convert.ToInt32(Math.Ceiling(getY(wiimote) + 3) * 255 / 12), 255);
+                    }
+                    if (getY(wiimote) > (yCal + 1))
+                    {
+                        //This should be Backwards
+                        speed = Math.Max(Convert.ToInt32(Math.Floor(getY(wiimote) - 3) * 255 / 12), -255);
+                    }
+                    if (getX(wiimote) > (xCal + 1))
+                    {
+                        turn = Math.Max(Convert.ToInt32(Math.Floor((getX(wiimote) - 3) * 20 / 12)), -20);
+                    }
+                    if (getX(wiimote) < (xCal - 1))
+                    {
+                        turn = Math.Min(Convert.ToInt32(Math.Ceiling((getX(wiimote) + 3) * 20 / 12)), 20);
+                    }
+                    Console.Write("X: ");
+                    Console.WriteLine(turn.ToString());
+                    Console.Write("Y: ");
+                    Console.WriteLine(speed.ToString());
+                    
+
+                    Dictionary<string, object> dick = new Dictionary<string, object>();
+                    dick.Add("speed", speed);
+                    dick.Add("steer", turn);
+                    dick.Add("aux", jump);
+
+
+                    string JsonString = JsonConvert.SerializeObject(dick);
+
+                    byte[] data = Encoding.ASCII.GetBytes(JsonString);
+                    socket.Send(data);
+                    System.Threading.Thread.Sleep(250);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_closeClick(object sender, FormClosedEventArgs e)
+        {
+            this.Close();
+            oThread.Abort();
+            
+            Application.Exit();
+            
+        }
+
+
+        leviathanserver oServer = new leviathanserver();
+        Thread oThread; 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oThread = new Thread(new ThreadStart(oServer.WiiLeviathanServer));
+            oThread.Start();
+            while (!oThread.IsAlive);
+            Thread.Sleep(1);
+            button1.Hide();
+
+        }
+    }
+}
