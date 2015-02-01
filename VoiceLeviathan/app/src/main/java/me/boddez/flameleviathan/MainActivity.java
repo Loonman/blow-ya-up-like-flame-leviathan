b4f1d4f89e5d6fea73b93b78351ee58f735232b3
snackthyme <sdboddez@ualberta.ca>
2015-01-31 22:39:08 -0700

    abusing logic

--- VoiceLeviathan/app/src/main/java/me/boddez/flameleviathan/MainActivity.java
+++ VoiceLeviathan/app/src/main/java/me/boddez/flameleviathan/MainActivity.java
@@ -1,6 +1,10 @@
 package me.boddez.flameleviathan;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.json.JSONObject;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,7 +13,6 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,17 +31,6 @@ public class MainActivity extends Activity {
         connectedView = (TextView) findViewById(R.id.connectedView);
         connectedView.setText("Not connected");
     }
-
-//    @Override
-//    public void onClick(View v) {
-//        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-//        try {
-//            startActivityForResult(i, REQUEST_OK);
-//        } catch (Exception e) {
-//            Toast.makeText(this, "Error initializing speech to text engine.", Toast.LENGTH_LONG).show();
-//        }
-//    }
 
     public void connect() {
         c = new Connection("192.168.1.100", 50007);
@@ -70,12 +62,170 @@ public class MainActivity extends Activity {
         if (requestCode==REQUEST_OK  && resultCode==RESULT_OK) {
             ArrayList<String> receivedSpeech = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
             ((TextView)findViewById(R.id.received_speech)).setText(receivedSpeech.get(0));
+            Map<String, String> toConvert = makeSense(receivedSpeech);
         }
     }
 
-//    private void makeSense(ArrayList<String> received) {
-//        String[] words = received.get(0).split("\\s+");
-//    }
+    private Map<String, String> makeSense(ArrayList<String> received) {
+        Map<String, String> dict = new HashMap<String, String>();
+        String[] words = received.get(0).split("\\s+");
+
+        boolean stop = true;
+        boolean moveIsActive = false;
+        boolean dontIsActive = false;
+        boolean turnIsActive = false;
+        boolean sentSpeed = false;
+        boolean sentTurn = false;
+        boolean positiveSpeed = false;
+        boolean setDirection = false; // forward vs backward
+        boolean sortaIsActive = false;
+        boolean reallyIsActive = false;
+        boolean youreAnIdiot = false;
+
+        for (String word: words) {
+
+            if (dontIsActive && !word.equals("don't")) continue;
+            if (stop) break;
+            if (youreAnIdiot) {
+                Toast.makeText(getApplicationContext(), "You're an idiot", Toast.LENGTH_LONG).show();
+                break;
+            }
+
+            switch (word) {
+                case "stop":
+                    stop = true;
+                    dict.put("speed", "0");
+                    break;
+
+                case "don't":
+                    dontIsActive = !dontIsActive;
+                    break;
+
+                case "move":
+                case "drive":
+                    if (dontIsActive) {
+                        stop = true;
+                    } else {
+                        moveIsActive = true;
+                    }
+                    break;
+
+                case "forward":
+                case "forwards":
+                    if (setDirection) {
+                        youreAnIdiot = true;
+                    } else {
+                        positiveSpeed = moveIsActive;
+                        setDirection = true;
+                    }
+                    break;
+
+                case "backward":
+                case "backwards":
+                    if (setDirection) {
+                        youreAnIdiot = true;
+                    } else {
+                        positiveSpeed = !moveIsActive;
+                        setDirection = true;
+                    }
+                    break;
+
+                case "sorta":
+                case "kinda":
+                case "relatively":
+                case "somewhat":
+                case "moderately":
+                case "pretty":
+                    if (reallyIsActive) {
+                        youreAnIdiot = true;
+                    } else {
+                        sortaIsActive = true;
+                    }
+                    break;
+
+                case "really":
+                case "super":
+                case "extra":
+                case "ultra":
+                case "mega":
+                    if (sortaIsActive) {
+                        youreAnIdiot = true;
+                    } else {
+                        reallyIsActive = true;
+                    }
+                    break;
+
+                case "fast":
+                case "quick":
+                case "quickly":
+                    if (sentSpeed) {
+                        youreAnIdiot = true;
+                        break;
+                    }
+                    if (sortaIsActive) {
+                        if (positiveSpeed) {
+                            dict.put("speed", "180");
+                        } else {
+                            dict.put("speed", "-180");
+                        }
+                    } else if (reallyIsActive) {
+                        if (positiveSpeed) {
+                            dict.put("speed", "255");
+                        } else {
+                            dict.put("speed", "-255");
+                        }
+                    }
+                    sentSpeed = true;
+                    break;
+
+                case "slow":
+                case "slowly":
+                    if (sentSpeed) {
+                        youreAnIdiot = true;
+                        break;
+                    }
+                    if (sortaIsActive) {
+                        if (positiveSpeed) {
+                            dict.put("speed", "100");
+                        } else {
+                            dict.put("speed", "-100");
+                        }
+
+                    } else if (reallyIsActive) {
+                        if (positiveSpeed) {
+                            dict.put("speed", "40");
+                        } else {
+                            dict.put("speed", "-40");
+                        }
+                    }
+                    sentSpeed = true;
+                    break;
+
+                case "turn":case "steer":
+                    turnIsActive = true;
+                    break;
+
+                case "left":
+                    if (sentTurn) {
+                        youreAnIdiot = true;
+                    } else {
+                        dict.put("steer","-30");
+                        sentTurn = true;
+                    }
+                    break;
+
+                case "right":
+                    if (sentTurn) {
+                        youreAnIdiot = true;
+                    } else {
+                        dict.put("steer","30");
+                        sentTurn = true;
+                    }
+                    break;
+            }
+        }
+        return dict;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
